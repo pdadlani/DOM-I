e75398250d478d8241cfd49b782f1cf50854c0f0
Priyanka Dadlani <dadlani.priyanka@gmail.com>
2019-07-09 12:16:32 -0700

    added timer functionality w/ stretch on the stretch completed.

---
+++ stretch-assignment/digital_timer/index.js
@@ -0,0 +1,49 @@
+const button = document.querySelector('button'),
+  secondTens = document.querySelector('#secondTens'),
+  secondOnes = document.querySelector('#secondOnes'),
+  msHundreds = document.querySelector('#msHundreds'),
+  msTens = document.querySelector('#msTens');
+
+  // variable to turn all elements of timer red when timer ends
+  timerDigits = document.querySelectorAll('.digit')
+
+// setting initial timer to zero
+let timer = 0;
+
+// variable to lock start/reset button
+let lock = false;
+
+button.addEventListener('click', event => {
+  if (!lock) {
+    // lock button upon start of timer
+    lock = true;
+
+    // setting / resetting timer to zero
+    timer = 0;
+
+    // removing red coloring using css class
+    timerDigits.forEach(digit => digit.classList.remove('redDigit'));
+    event.preventDefault();
+
+    // timer functionality
+    const interval = setInterval(() => {
+      timer += 10;
+      msTens.textContent = (timer / 10) % 10;
+      msHundreds.textContent = Math.floor(timer / 100) % 10;
+      secondOnes.textContent = Math.floor(timer / 1000) % 10;
+      secondTens.textContent = Math.floor(timer / 10000) % 10;
+
+      // once timer hits maximum (10 seconds in this case)
+      if (timer >= 10000) {
+
+        // unlock button, clear, and set text to "Reset"
+        lock = false;
+        clearInterval(interval);
+        button.innerHTML = 'Reset';
+        
+        // timerDigits.forEach(digit => digit.style.color='red');
+        timerDigits.forEach(digit => digit.classList.add('redDigit'));
+      }
+    }, 10);
+  }
+});
